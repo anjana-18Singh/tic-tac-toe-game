bc67a45ef414ffbf88aefc5a73532e0c7e6435dd
anjana-18Singh <121855141+anjana-18Singh@users.noreply.github.com>
2024-01-04 16:29:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+
+//this is used to access the boxes buttonhn 
+let boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-conatiner");
+let msg=document.querySelector("msg");
+
+let turnO = true;//p1,P2
+
+const winpattern=[[0,1,2],
+                  [0,3,6],
+                  [0,4,8],
+                  [1,4,7],
+                  [2,5,8],
+                  [2,4,6],
+                  [3,4,5],
+                  [6,7,8]
+                ];
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        console.log("box was clicked");
+        if(turnO){
+            box.innerText="X";
+            turnO=false;
+        }else{
+            box.innerText="O";
+            turnO=true;
+        }
+        box.disabled=true;
+
+        
+        checkWinner();
+        
+    });
+   
+});
+
+const showWinner=(winner) => {
+    msg.innerText=`congratulation,Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+};
+
+const checkWinner = () => {
+    for (let pattern of winpattern) {
+      let pos1val=boxes[pattern[0]].innerText;
+      let pos2val=boxes[pattern[1]].innerText;
+      let pos3val=boxes[pattern[2]].innerText;
+
+    if(pos1val!= "" && pos2val!= "" && pos3val!=""){
+        if(pos1val === pos2val && pos2val === pos3val){
+            console.log("winner",pos1val);
+            showWinner(pos1val);
+        }
+      }
+    }
+};
